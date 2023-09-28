f2d3b1f91a11d4452496cc4a4758f4c8571b68a9
Chad Lee <git@chadly.net>
2023-09-28 09:48:55 -0500

    🐛 fix: remove model auto-download

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,17 +2,6 @@
 
 set -e
 
-if [ ! -f "/home/runner/ComfyUI/models/.download-complete" ] ; then
-	echo "########################################"
-	echo "Downloading models"
-	echo "########################################"
-
-	aria2c --allow-overwrite=false --auto-file-renaming=false --continue=true \
-		--max-connection-per-server=5 --input-file=/home/scripts/download.txt
-
-	touch /home/runner/ComfyUI/models/.download-complete
-fi
-
 echo "########################################"
 echo "Starting ComfyUI..."
 echo "########################################"
